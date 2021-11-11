3d1796f8e359c16db28c63d698ca0d56371f2ff4
Thitat Auareesuksakul <thitat@fluxthitat.me>
2021-11-11 22:17:11 +0700

    fix: fix assetPrefix for github pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+const DEBUG = process.env.NODE_ENV === "development";
+
 module.exports = {
   reactStrictMode: true,
-}
+  assetPrefix: !DEBUG ? "/vouchergen" : "",
+};
